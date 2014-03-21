daf24234e08512783828e32031b09dc44176ef81
qbantek <qbantek@gmail.com>
2014-03-21 17:18:38 -0400

    generating a random secret for dev purposes.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,31 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+#noinspection SpellCheckingInspection
+require 'securerandom'
+
+
+def get_secret_token
+  token_file = Rails.root.join('.secret')
+
+  if File.exist?(token_file)
+    token = File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+  end
+
+  token
+end
+
+
+def secret_token
+  ENV['SECRET_TOKEN'].blank? ? get_secret_token : ENV['SECRET_TOKEN']
+end
+
+
+Themeable::Application.config.secret_token = secret_token
